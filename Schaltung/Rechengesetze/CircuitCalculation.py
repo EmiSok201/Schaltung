f2b6eb0c653515ce352b576416618731d0ca18c6
CodeArSenPai <40718371+CodeArSenPai@users.noreply.github.com>
2023-12-13 17:01:35 +0100

    commit

--- Schaltung/Rechengesetze/CircuitCalculation.py
+++ Schaltung/Rechengesetze/CircuitCalculation.py
@@ -5,10 +5,13 @@ class CircuitCalculation:
         self.resistor2 = resistor2
 
     def calculate_series_circuit(self):
+
         resistor = self.resistor1 + self.resistor2
+
         return resistor
 
-    def calculate_parallel_circuit(self):
-        resistor = (self.resistor1 * self.resistor2)/ (self.resistor1 + self.resistor2)
-        return resistor
+
+def calculate_parallel_circuit(self):
+    resistor = (self.resistor1 * self.resistor2) / (self.resistor1 + self.resistor2)
+    return resistor
 
--- Schaltung/main.py
+++ Schaltung/main.py
@@ -13,26 +13,20 @@ class Main:
         r1 = resistor.get_resistor()
         resistor2 = Resistor(10)
         r2 = resistor2.get_resistor()
+
         power_source = PowerSource(30)
         i0 = power_source.get_ampere()
+
         voltage_source = VoltageSource(30)
         u0 = voltage_source.get_voltage()
 
         # Reihenschaltung von r1 und r2
 
-        # Berechnung Gesamtwiderstand
+        # Gesamtwiderstand (In Reihe geschaltet)
         resistor_ges = r1 + r2
+        print("R1 = ", r1, "Ohm")
+        print("R2 = ", r2, "Ohm")
         print("Gesamtwiderstand = ", resistor_ges, "Ohm")
-
-        # Print Ursprungsspannung
-        print("V0 =", u0, "V")
-
-        # Berechnung der Teilspannungen
-        u1 = VoltageDivider(r1, r2, u0).calculate_u1()
-        print("U1 =", u1, "V")
-
-        u2 = VoltageDivider(r1, r2, u0).calculate_u2()
-        print("U2 =", u2, "V")
 
         # Bestimmung des Stroms
         i1 = i0
@@ -41,6 +35,16 @@ class Main:
         print("I1 =", i1, "A")
         print("I2 =", i2, "A")
 
+       # Print Ursprungsspannung
+        print("V0 =", u0, "V Eingangsspannung")
+
+        print("Berechnete Ausgangsspannungen:")
+        # Berechnung der Teilspannungen
+        u1 = VoltageDivider(r1, r2, u0).calculate_u1()
+        print("U1 =", u1, "V")
+
+        u2 = VoltageDivider(r1, r2, u0).calculate_u2()
+        print("U2 =", u2, "V")
 
 if __name__ == '__main__':
     main = Main()

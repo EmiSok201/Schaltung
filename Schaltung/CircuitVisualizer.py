17f9c938c44524cc14376076fd8caf5cebfa7fbf
CodeArSenPai <40718371+CodeArSenPai@users.noreply.github.com>
2024-01-24 16:47:20 +0100

    commit

--- Schaltung/CircuitVisualizer.py
+++ Schaltung/CircuitVisualizer.py
@@ -18,17 +18,21 @@ class CircuitVisualizer:
         voltage_position = (0, self.square_size / 2)
         self.add_component('voltage_source', 'U', voltage_position)
 
+        # Calculate the spacing for resistors
+        spacing = self.square_size / (len(resistors) + 1)
+
         # Place resistors in the circuit
-        x_offset = 1
         for i, resistor in enumerate(resistors):
-            if isinstance(resistor,Parallel):
-                position = (x_offset-0.6, self.square_size / 2)
-                self.add_component('resistor', f'R{i+1} ({resistor.get_ohm()}Ω)', position, orientation='vertical')
-                x_offset += 1
+            x_position = (i + 1) * spacing
+            if isinstance(resistor, Parallel):
+                # Place parallel resistors vertically on the right
+                position = (x_position, self.square_size / 2)
+                orientation = 'vertical'
             else:
-                position = (x_offset, self.square_size)
-                self.add_component('resistor', f'R{i+1} ({resistor.get_ohm()}Ω)', position)
-                x_offset += 1
+                # Place other resistors horizontally along the top
+                position = (x_position, self.square_size)
+                orientation = 'horizontal'
+            self.add_component('resistor', f'R{i+1} ({resistor.get_ohm()}Ω)', position, orientation)
 
     def draw_resistor(self, position, label, orientation):
         if orientation == 'horizontal':
@@ -42,47 +46,40 @@ class CircuitVisualizer:
         self.ax.text(position[0], position[1], label, ha='center', va='center')
 
     def draw_wires(self):
+        # Adjust the right boundary based on the last resistor's position
+        right_boundary_x = max(comp[2][0] for comp in self.components if comp[0] == 'resistor')
+
         # Draw the square frame of the circuit
         square_coords = [
             (0, 0), (0, self.square_size),
-            (self.square_size, self.square_size), (self.square_size, 0),
+            (right_boundary_x, self.square_size), (right_boundary_x, 0),
             (0, 0)
         ]
         self.ax.plot(*zip(*square_coords), 'k-')
 
-        #voltage source is the first component and the first resistor is the second
+        # Draw wires from the voltage source to the first resistor
         if len(self.components) > 1:
             v_source = self.components[0]
-            first_resistor = self.components[1]
+            first_resistor = self.components[0]
 
-            # Draw wire from voltage source (U0) to the bottom left corner of the square
-            self.ax.plot([v_source[2][0], 0], [v_source[2][1], 0], 'k-')
+            # Draw wire from voltage source to the first series resistor
+            self.ax.plot([v_source[2][0], first_resistor[2][0]], [v_source[2][1], first_resistor[2][1]], 'k-')
 
-            # Draw wire from bottom left corner to bottom right corner (following the boundary)
-            self.ax.plot([0, first_resistor[2][0]], [0, 0], 'k-')
+        # Connect all series resistors
+        for i in range(1, len(self.components)):
+            component = self.components[i]
+            if component[0] == 'resistor' and component[3] == 'horizontal':
+                if i > 1:  # If it's not the first resistor, connect it to the previous one
+                    prev_component = self.components[i - 1]
+                    self.ax.plot([prev_component[2][0], component[2][0]], [prev_component[2][1], component[2][1]], 'k-')
 
-            # Draw wire up to the first resistor
-            self.ax.plot([first_resistor[0][0], first_resistor[0][0]], [0, first_resistor[2][1]], 'k-')
-
-        # Draw the wires for the rest of the components based on their positions
-        for i, component in enumerate(self.components[2:], start=2):  # Skip the voltage source and first resistor
-            ctype, label, pos, ori = component
-            prev_pos = self.components[i - 1][2]
-            if ctype == 'resistor':
-                if ori == 'horizontal':
-                    # Draw wire from the previous component to this resistor
-                    self.ax.plot([prev_pos[0], pos[0]], [prev_pos[1], pos[1]], 'k-')
-                else:
-                    # Draw wire from the top of the square to the parallel resistor
-                    self.ax.plot([pos[0], pos[0]], [self.square_size, pos[1]], 'k-')
-                    # Draw wire from the parallel resistor to the bottom of the square
-                    self.ax.plot([pos[0], pos[0]], [pos[1], 0], 'k-')
-
-                    # Draw wire from the top of the square to the parallel resistor
-                    self.ax.plot([pos[0], pos[0]], [self.square_size, pos[1]], 'k-')
-                    # Draw wire from the parallel resistor to the bottom of the square
-                    self.ax.plot([pos[0], pos[0]], [pos[1], 0], 'k-')
-
+        # Connect parallel resistors
+        for i in range(1, len(self.components)):
+            component = self.components[i]
+            if component[0] == 'resistor' and component[3] == 'vertical':
+                # Connect to the top and bottom of the square
+                self.ax.plot([component[2][0], component[2][0]], [self.square_size, component[2][1]], 'k-')  # Top wire
+                self.ax.plot([component[2][0], component[2][0]], [0, component[2][1]], 'k-')  # Bottom wire
 
 
     def draw(self):

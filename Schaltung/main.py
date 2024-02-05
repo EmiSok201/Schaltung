b591deb328e7590faecd910b4925873343ef974d
Emilia <sokolemilia78@gmail.com>
2024-02-05 22:15:58 +0100

    Commit

--- Schaltung/main.py
+++ Schaltung/main.py
@@ -31,32 +31,8 @@ class Main:
         currents_erste_schaltung = ElectricalParameters.calculate_currents([resistor1, resistor2, resistor3], [u_1, u_2, u_3])
         Print.print_currents("Erste Schaltung", **currents_erste_schaltung)
 
-        # Second circuit
-        r1 = Resistor(200)
-        r2 = Resistor(300)
-        r3 = Resistor(500)
-        r4 = Resistor(400)
-
-        rserie = Series(r3, r4)
-        rparallel = Parallel(r2, rserie)
-        rges = Series(r1, rparallel)
-
-        Resistor.print("Zweite Schaltung", r1, r2, r3, r4, rserie, rparallel, rges)
-
-        u0 = VoltageSource(20)
-        u1 = VoltageSource(18.31)
-        u2 = VoltageSource(11.68)
-        u3 = u4 = u2
-
-        # Print voltages
-        Print.print_voltages("Zweite Schaltung", U0=u0, U1=u1, U2=u2, U3=u3, U4=u4)
-
-        currents_zweite_schaltung = ElectricalParameters.calculate_currents([r1, r2, r3, r4], [u1, u2, u3, u4])
-        Print.print_currents("Zweite Schaltung", **currents_zweite_schaltung)
-
         # Visualization
-        Main.visualize_circuit(u_0, [resistor1, r_parallel, r3, r_parallel])
-        Main.visualize_circuit(u0, [r1, rparallel, r4])
+        Main.visualize_circuit(u_0, [resistor1, r_parallel, resistor3, r_parallel])
 
     @staticmethod
     def visualize_circuit(voltage_source, resistors):
